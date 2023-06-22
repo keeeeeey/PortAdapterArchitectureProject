c2547a588bdd4678b83d7ba2542db7508e4ac2f1
keeeeeey <sseioul@naver.com>
2023-06-22 15:31:12 +0900

    Feat: create Account domain

--- src/main/java/com/example/buckpal/account/domain/Account.java
+++ src/main/java/com/example/buckpal/account/domain/Account.java
@@ -1,4 +1,61 @@
 package com.example.buckpal.account.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Account {
+
+    private AccountId id;
+    private Money baselineBalance;
+    private ActivityWindow activityWindow;
+
+    public Money calculateBalance() {
+        return Money.add(
+                this.baselineBalance,
+                this.activityWindow.calculateBalance(this.id)
+        );
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+        if (!mayWithdraw(money)) {
+            return false;
+        }
+
+        Activity withdrawal = new Activity(
+                this.id,
+                this.id,
+                targetAccountId,
+                LocalDateTime.now(),
+                money
+        );
+
+        this.activityWindow.addActivity(withdrawal);
+        return true;
+    }
+
+    private boolean mayWithdraw(Money money) {
+        return Money.add(
+                this.calculateBalance(),
+                money.negate()
+        ).isPositive();
+    }
+
+    public boolean deposit(Money money, AccountId sourceAccountId) {
+        Activity deposit = new Activity(
+                this.id,
+                sourceAccountId,
+                this.id,
+                LocalDateTime.now(),
+                money
+        );
+
+        this.activityWindow.addActivity(deposit);
+        return true;
+    }
 }
